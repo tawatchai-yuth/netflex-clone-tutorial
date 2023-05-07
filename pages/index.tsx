ce83cf9462a148fae8fa82fbc39ceeabd7eff2f4
tawatchai-yuth <tawatchai_yuth@hotmail.com>
2023-05-07 12:37:06 +0700

    test

---
+++ pages/index.tsx
@@ -0,0 +1,48 @@
+
+import useCurrentUser from '@/hooks/useCurrentUser';
+import { NextPageContext } from 'next';
+import { getSession } from 'next-auth/react';
+
+import Navbar from '@/components/Navbar';
+import Billboard from '@/components/Billboard';
+import MoviesList from '@/components/MoviesList';
+import useMoviesList from '@/hooks/useMoviesList';
+import useFavorites from '@/hooks/useFavorites';
+import InfoModal from '@/components/InfoModal';
+import useInfoModal from '@/hooks/useInfoModal';
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false
+      }
+    }
+  }
+
+  return {
+    props: {}
+  }
+}
+
+export default function Home() {
+
+  const { data: movies = [] } = useMoviesList();
+  const { data: favorites = [] } = useFavorites();
+  const { isOpen, closeModal } = useInfoModal();
+
+  return (
+    <>
+      <InfoModal visible={isOpen} onClose={closeModal} />
+      <Navbar />
+      <Billboard />
+      <div className="pb-40">
+        <MoviesList title="Trending Now" data={movies} />
+        <MoviesList title="My List" data={favorites} />
+      </div>
+    </>
+  )
+}
